6afd83848bb17fd97f55a46d0cbd46e8a3fd544d
jpm2000 <124071555+jpm2000@users.noreply.github.com>
2023-11-01 16:18:37 -0500

    Changed the open ai API key

--- demo.py
+++ demo.py
@@ -20,7 +20,7 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
