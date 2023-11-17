4b03963431fc5e8b96657c399a2cc9d2f6c496af
jpm2000 <124071555+jpm2000@users.noreply.github.com>
2023-11-17 11:24:27 -0500

    finished the model to calculate present and future values

--- amortizacion/inputs.py
+++ amortizacion/inputs.py
@@ -10,34 +10,41 @@ cuota = ()
 saldo_ant = ()
 amortizacion = ()
 
-
 def escenario():
     print('Los modelos financiero disponibles son:')
     print('1. Valor futuro')
     print('2. Valor presete')
     matfin = input('Cual es el modelo de matematica financiero que quieres que calcule: ') 
-    if matfin == '1. Valor futuro':
+    if matfin == '1':
         presente = float(input("¿Cuánto quieres invertir?: "))
-        periodos = float(input('¿Por cuanto tiempo quieres dejar la inversión?: '))
+        periodos = input('¿Por cuanto tiempo quieres dejar la inversión?: ')
         valores = periodos.split()
         if len(valores) == 2:
-            n = valores[0]
-            periodicidad = valores[1]
-        tasa = float(input('¿De cuánto es la tasa de interés? (sin el signo de porcentage): '))/100
-        periodo_tasa = input('¿Qué tipo de periodos tiene la tasa? (mensual, trimestral, semestral, anual): ')
-        valor = presente*((1+tasa)**n)
-        analisis = f'El valor futuro de tu inversión de {presente} por {peridos} '
-    elif matfin == '2. Valor presete':
+            n = float(valores[0])
+        tasa = input('¿De cuánto es la tasa de interés? (sin el signo de porcentage): ')
+        interes = tasa.split()
+        if len(interes) == 2:
+            i = float(interes[0])/100 
+        valor = presente*((1+i)**n)
+        analisis = f'Si inviertes {presente} a {periodos}, con una tasa de {tasa} el valor futuro de tu inversión será: {valor}'
+        # analisis = f'El valor futuro de tu inversión de {presente} por {peridos} '
+    elif matfin == '2':
         futuro = float(input("¿Cuánto quieres tener acumulado en tu inversión?: "))
-        periodos = float(input('¿Por cuanto tiempo quieres dejar la inversión?: '))
-        tasa = float(input('¿De cuánto es la tasa de interés? (sin el signo de porcentage): '))/100
-        periodo_tasa = input('¿Qué tipo de periodos tiene la tasa? (mensual, trimestral, semestral, anual): ')
-        valor = futuro/((1+tasa)**periodos)
+        periodos = input('¿Por cuanto tiempo quieres dejar la inversión?: ')
+        valores = periodos.split() 
+        if len(valores) == 2:
+            n = float(valores[0])
+        tasa = input('¿De cuánto es la tasa de interés? (sin el signo de porcentage): ')
+        interes = tasa.split()
+        if len(interes) == 2:
+            i = float(interes[0])/100
+        valor = futuro/((1+i)**n)
+        analisis = f'Si quieres tener {futuro} en {periodos}, con una tasa de {tasa}, hoy tienes que invertir: {valor}'
     else:
-        valor = 'Prueba otra vez'
-    return valor
+        analisis = 'Prueba otra vez'
+    return analisis
 
-
+print("")
 print(escenario())
 
 
