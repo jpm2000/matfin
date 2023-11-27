69a01318a3bf5e02c2c733904b6811a091e6b740
jpm2000 <124071555+jpm2000@users.noreply.github.com>
2023-11-27 17:32:32 -0500

    up the temperature of the LLM

--- app.py
+++ app.py
@@ -22,14 +22,13 @@ def main():
     if csv_file is not None:
 
         agent = create_csv_agent(
-            OpenAI(temperature=0), csv_file, verbose=True)
+            OpenAI(temperature=1.5), csv_file, verbose=True)
 
         user_question = st.text_input("Ask a question about your CSV: ")
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
                 st.write(agent.run(user_question))
-
 
 if __name__ == "__main__":
     main()
